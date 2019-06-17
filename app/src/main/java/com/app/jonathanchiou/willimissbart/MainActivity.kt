65ed2804452292552515948829ddad92b675679a
Jonathan Chiou <jchio799@gmail.com>
2019-06-16 18:07:27 -0700

    Implemented BottomNavigationViewManager
    This handles correlating between a BottomNavigationView and a fragment backstack.
    
    >mrw I have to work with these 2 terrible apis https://www.youtube.com/watch?v=SLEdsI731J4

--- app/src/main/java/com/app/jonathanchiou/willimissbart/MainActivity.kt
+++ app/src/main/java/com/app/jonathanchiou/willimissbart/MainActivity.kt
@@ -4,29 +4,50 @@ import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.app.jonathanchiou.willimissbart.BottomNavigationViewManager.FragmentFactory
 import com.app.jonathanchiou.willimissbart.stations.StationsManager
 import com.app.jonathanchiou.willimissbart.trips.RealTimeTripFragment
 import com.app.jonathanchiou.willimissbart.trips.TripManager
 import com.app.jonathanchiou.willimissbart.trips.TripSelectionFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    @BindView(R.id.bottom_navigationview)
+    lateinit var bottomNavigationView: BottomNavigationView
+
     lateinit var tripManager: TripManager
+
+    lateinit var bottomNavigationViewManager: BottomNavigationViewManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
 
         supportActionBar?.hide()
 
         StationsManager.initialize(this)
 
         tripManager = TripManager(PreferenceManager.getDefaultSharedPreferences(this))
+
+        bottomNavigationViewManager = BottomNavigationViewManager(
+            bottomNavigationView,
+            supportFragmentManager,
+            R.id.parent,
+            object: FragmentFactory {
+
+                override fun createFragment(itemId: Int): Fragment {
+                    return RealTimeTripFragment()
+                }
+            }
+        )
+
         if (tripManager.originAbbreviation != null && tripManager.destinationAbbreviation != null) {
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.parent, RealTimeTripFragment())
-                .commit()
+            bottomNavigationViewManager.setSelectedItem(R.id.trips)
         } else {
             supportFragmentManager
                 .beginTransaction()
@@ -50,5 +71,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if (!bottomNavigationViewManager.onBackPressed()) {
+            finish()
+        }
     }
 }
