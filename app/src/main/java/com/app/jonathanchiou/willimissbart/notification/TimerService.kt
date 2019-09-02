bb52608056c1fd35cd2bdbdc9f42eda2e0b55476
Jonathan Chiou <jchio799@gmail.com>
2019-09-02 12:49:53 -0700

    Countdown now persists even after the app is closed
    Tested post and pre Oreo

--- app/src/main/java/com/app/jonathanchiou/willimissbart/notification/TimerService.kt
+++ app/src/main/java/com/app/jonathanchiou/willimissbart/notification/TimerService.kt
@@ -22,67 +22,86 @@ class TimerService : Service() {
     private val timerSubject = PublishSubject.create<Long>()
     private val compositeDisposable = CompositeDisposable()
 
+    private var lastStartTime = -1L
+
+    private val dismissIntent by lazy {
+        PendingIntent.getService(
+            this,
+            0,
+            Intent(this, TimerService::class.java)
+                .setAction(ACTION_DISMISS),
+            PendingIntent.FLAG_ONE_SHOT)
+    }
+
     init {
         compositeDisposable.add(
             timerSubject
+                .doOnNext {
+
+                }
                 .switchMap { value ->
-                    Observable.intervalRange(1, value + 1, 0, 1, TimeUnit.SECONDS)
-                        .map { value + 1 - it }
+                    Observable.intervalRange(1, value, 1, 1, TimeUnit.SECONDS)
+                        .map { value - it }
                         .observeOn(AndroidSchedulers.mainThread())
                 }
                 .subscribe { time ->
                     Log.d("Timer", time.toString())
 
-                    val notificationManager = getNotificationManager()
-
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
-                        && notificationManager.getNotificationChannel(CHANNEL_ID) == null) {
-                        notificationManager.createNotificationChannel(
-                            NotificationChannel(
-                                CHANNEL_ID,
-                                "BartBuddy real time trip timer",
-                                NotificationManager.IMPORTANCE_HIGH
-                            )
+                    val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                        .addAction(
+                            R.drawable.ic_train_icon,
+                            this.getString(R.string.cancel),
+                            dismissIntent
                         )
-                    }
+                        .setContentTitle(time.toTimerText())
+                        .setSmallIcon(R.drawable.ic_train_icon)
+                        .build()
 
-                    notificationManager.notify(
-                        NOTIFICATION_ID,
-                        NotificationCompat.Builder(this, CHANNEL_ID)
-                            .setSmallIcon(R.drawable.ic_train_icon)
-                            .setContentTitle("pizza")
-                            .setContentText(time.toTimerText())
-                            .setOngoing(true)
-                            .addAction(
-                                R.drawable.ic_train_icon,
-                                this.getString(R.string.cancel),
-                                PendingIntent.getService(
-                                    this,
-                                    NOTIFICATION_ID,
-                                    Intent(this, TimerService::class.java)
-                                        .setAction(ACTION_DISMISS),
-                                    PendingIntent.FLAG_CANCEL_CURRENT
-                                )
-                            )
-                            .build()
-                    )
+                    startForeground(NOTIFICATION_ID, notification)
                 }
         )
     }
 
     override fun onBind(intent: Intent?): IBinder? = null
 
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        when (intent.action) {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.action) {
             ACTION_SHOW -> {
                 val duration = intent.getLongExtra(DURATION_ARG, -1)
                 if (duration > -1) {
+                    val notificationManager = getNotificationManager()
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+                        && notificationManager.getNotificationChannel(CHANNEL_ID) != null) {
+                        notificationManager.createNotificationChannel(
+                            NotificationChannel(
+                                CHANNEL_ID,
+                                "BartBuddy real time trip timer",
+                                NotificationManager.IMPORTANCE_DEFAULT
+                            )
+                                .apply {
+                                    setShowBadge(false)
+                                }
+                        )
+                    }
+                    val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                        .addAction(
+                            R.drawable.ic_train_icon,
+                            this.getString(R.string.cancel),
+                            dismissIntent
+                        )
+                        .setContentTitle(duration.toTimerText())
+                        .setSmallIcon(R.drawable.ic_train_icon)
+                        .build()
+
+                    startForeground(NOTIFICATION_ID, notification)
+
                     timerSubject.onNext(duration)
                 }
             }
             ACTION_DISMISS -> {
                 (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).cancel(NOTIFICATION_ID)
-                this.stopService(intent)
+                stopService(intent)
             }
         }
 
@@ -90,16 +109,18 @@ class TimerService : Service() {
     }
 
     override fun stopService(name: Intent?): Boolean {
+        stopForeground(true)
         compositeDisposable.clear()
         return super.stopService(name)
     }
 
     companion object {
 
-        const val CHANNEL_ID = "bart-buddy-channel"
+        const val CHANNEL_ID = "will_I_miss_bart_real_time_trip_channel"
 
         const val NOTIFICATION_ID = 73
 
+        const val ACTION_START = "BartBuddy.start"
         const val ACTION_SHOW = "BartBuddy.show"
         const val ACTION_DISMISS = "BartBuddy.dismiss"
 
@@ -115,11 +136,19 @@ class TimerService : Service() {
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         fun Context.startRealTimeTripTimer(minutes: Long) {
-            this.startService(
-                Intent(this, TimerService::class.java)
-                    .setAction(ACTION_SHOW)
-                    .putExtra(DURATION_ARG, minutes * 60)
-            )
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                this.startForegroundService(
+                    Intent(this, TimerService::class.java)
+                        .setAction(ACTION_SHOW)
+                        .putExtra(DURATION_ARG, minutes * 60)
+                )
+            } else {
+                this.startService(
+                    Intent(this, TimerService::class.java)
+                        .setAction(ACTION_SHOW)
+                        .putExtra(DURATION_ARG, minutes * 60)
+                )
+            }
         }
     }
 }
